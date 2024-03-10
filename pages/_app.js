c8449964a32cb5a45788e0c8c211c2c07425980d
jaja2302~ <valentinojaja@gmail.com>
2024-03-10 10:46:00 +0700

    first

---
+++ pages/_app.js
@@ -0,0 +1,27 @@
+import '../styles/globals.css';
+
+import Layout from "../components/Layout";
+import { AnimatePresence, motion } from 'framer-motion'
+import Transition from '../components/Transition'; // Change from { Transition } to Transition
+import { useRouter } from 'next/router';
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  return (
+    <Layout>
+      <AnimatePresence mode='wait'>
+        <motion.div key={router.route} className='h-full'>
+          <Transition/>
+        <Component {...pageProps} />
+        </motion.div>
+      
+      </AnimatePresence>
+   
+    </Layout>
+
+  );
+
+ 
+}
+
+export default MyApp;
